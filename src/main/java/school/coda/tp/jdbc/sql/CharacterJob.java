2bb32e2f1e46e81213692bd3bce34888043432a0
Marc Bouvier <m.bouvier.dev@gmail.com>
2026-03-02 22:56:04 +0100

    correction tp partie 1

--- src/main/java/school/coda/tp/jdbc/sql/CharacterJob.java
+++ src/main/java/school/coda/tp/jdbc/sql/CharacterJob.java
@@ -1,23 +1,25 @@
 package school.coda.tp.jdbc.sql;
 
+import java.util.Objects;
+
 public enum CharacterJob {
     WARRIOR("Warrior"),
     PRIEST("Priest"),
     PALADIN("Paladin"),
     MAGE("Mage"),
     THIEF("Thief"),
-    TOURIST("Tourjst"),
+    TOURIST("Tourist"),
     RPG_CHARACTER(null);
 
-    public String jobName;
+    public final String name;
 
-    CharacterJob(String jobName) {
-        this.jobName = jobName;
+    CharacterJob(String name) {
+        this.name = name;
     }
 
     public static CharacterJob fromName(String name) {
         for (CharacterJob characterJob : values()) {
-            if (characterJob.jobName.equals(name)) {
+            if (Objects.equals(characterJob.name, name)) {
                 return characterJob;
             }
         }
